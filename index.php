6b15a9bcbc6349873d40b61779a32374568c9e2f
Eliabe-Ribeiro-22 <eliaberibeiro06@hotmail.com>
2022-05-29 20:10:00 -0300

    Tranformando o index.html em index.php

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title>PALAVRA DE VIDA</title>
+
+	<link rel="stylesheet" type="text/css" href="Assets/css/styles.css">
+
+	<!--Código para site gerador de ícones -->
+	<script src="https://kit.fontawesome.com/ea3f2aed2c.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+	<header>
+		<nav class="NavHeader">
+			<ol>
+				<li>
+					<a class="aHeader"alt="Logo do Site Palavra Viva" title="Associação Palavra de Vida">Logo do Site Palavra Viva</a>
+				</li>
+
+				<li>
+					<a class="aHeader" id="HeaderQuemSomos">Quem Somos</a>
+				</li>
+				
+				<li>
+					<a class="aHeader" id="BotaoHamburguer"> <i class="fa-solid fa-bars"></i></a>
+				</li>
+
+				<li>
+					<a class="aHeader"id="HeaderContato">Entre em Contato Conosco</a>
+				</li>
+
+				<li>
+					<a class="aHeader"id="HeaderSejaMantenedor">Seja um Mantenedor do Projeto</a>
+				</li>
+			</ol>
+		</nav>
+	</header>
+	
+	<main>
+		<section>
+			<div>
+				<span>
+					<h1 title="Associação Palavra de Vida">SITE ASSOCIAÇÃO PALAVRA DE VIDA - WEB</h1>
+ 					
+ 					<h2 class="h2VenhaSerMantenedor"> <em> <strong> Venha ser um Mantenedor deste Projeto ! </strong> </em> </h2>
+		    </aside>	
+		</section>
+		<section class="QuemSomos">
+			<article>
+				<div class="sectionWebSite" id="ArteQuemSomos">
+					<h2 title="Associação Palavra de Vida">Associação Palavra de Vida</h2>
+					<p>
+					<b>	
+					Somos uma entidade não-governamental, que busca distribuir literaturas a população, sem distinção de sexo, religião, classe social, etc. Temos objetivo de arrecadar fundos para adquirir literaturas.
+					</b>
+					</p>
+				</div>	
+
+		<!-- Botão para conversar com a instituição pelo Whats'App. -->
+		<script>window.rwbp={email:'pr.volni@hotmail.com',phone:'+55 49 988477611',message:'Olá, fale conosco pelo What's App para saber mais informações!',lang:'pt-BR'}</script>
+
+		<script defer async src='https://duz4dqsaqembt.cloudfront.net/client/whats.js'></script>
+	</article>
+	<footer>
+		<div>
+			<h6 class="rodapé" id="Desenvolvedor">Este site foi desenvolvido por Eliabe Ribeiro Mota.</h6>
+		</div>
+
+		<div>
+			<h6 class="rodapé">Copyright © 2021-2022 Sistemas Eliabe. Todos os Direitos Reservados.</h6>
+		</div>
+	</footer>
+</body>
+</html>
