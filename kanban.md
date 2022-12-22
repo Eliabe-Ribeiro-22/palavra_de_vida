351bf88e0c492c99be522aafbce3b71655a426ad
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-12-21 23:06:24 -0300

    CSS - Estilização da página inicial

--- kanban.md
+++ kanban.md
@@ -1,3 +1,5 @@
 # To do
 
-envio de mensagem por email - não funcionando
+- envio de mensagem por email - não funcionando
+- trocar conteúdos de PROJETOS -> Divulgação
+- estilizar conteúdos da página principal
--- resources/views/igreja/inicio.blade.php
+++ resources/views/igreja/inicio.blade.php
@@ -14,243 +14,86 @@
 
         </video>
     </div>
-    <a href="#">Seja um sócio-mantenedor deste projeto!</a>
-
+    
+    <div class="projetos-igreja">
+        <h2>Quem somos</h2>
+        <h3>Somos uma entidade não-governamental, que busca distribuir literaturas a população</h3>
+    </div>
+    
+    <div class="projetos-igreja">
         <h2>Objetivo</h2>
+            <h3>
+                Associação que visa Arrecadar fundos financeiros para aquisição de literaturas, tais como:
+            </h3>  
+                <ul class="projeto-lista">  
+                    <li><a>Bíblias</a></li>
+                            
+                    <li><a>Livros</a></li>
+                            
+                    <li><a>Revistas</a></li>
+                            
+                    <li><a>Folhetos</a></li>
+                            
+                    <li><a>Apostilas</a></li>
+                            
+                    <li><a>Livretos para crianças, adolescentes, jovens e adultos</a></li>
+                </ul>
 
-        <p>
-                        <b>
-                        Associação que visa Arrecadar fundos financeiros para aquisição de literaturas, tais como:<br>
-                        </b>    
-                    
-                        <ul>
-                            <li>
-                                <a>
-                    
-                                Bíblias
-                                
-                                </a>
-                            </li>
-                            
-                            <li>
-                                <a>
-                                Livros
-                                </a>
-                            </li>
-                            
-                            <li>
-                                <a>
-                                Revistas
-                                </a>
-                            </li>
-                            
-                            <li>
-                                <a>
-                                    Folhetos
-                                </a>
-                            </li>
-                            
-                            <li>
-                                <a>
-                                Apostilas
-                                </a>
-                            </li>
-                            
-                            <li>
-                                <a>
-                                Livretos para crianças, adolescentes, jovens e adultos
-                                </a>
-                            </li>
-                        </ul>
-
-                        <b>
-                        Sendo que todos os materiais adquiridas serão distribuídos gratuitamente a toda a sociedade, através de seus associados contribuintes.
-                        </b>
-                    </p>
+            <h4>
+                Sendo que todos os materiais adquiridas serão distribuídos gratuitamente a toda a sociedade, através de seus associados contribuintes.
+            </h4>
                 
-                <h2 >Missão</h2>
-                <p>
-                    <!-- Texto pronto -->
-                    <strong>
-                        Levar conhecimento, cultura, educação, fé, lazer e entretenimento! 
-                    </strong>
-                </p>
+        </div>
+
+        <div class="projetos-igreja">     
+            <h2>Missão</h2>
+            <h3>
+                Levar conhecimento, cultura, educação, fé, lazer e entretenimento! 
+            </h3>
 
 
-                <h2 class="visao">Visão</h2>
-                <p>
-                    <strong>
-                        <!-- Texto Pronto -->
-                    Atender pessoas de todas as idades com literaturas educativas, de auto-ajuda e proporcionando bem-estar a todos.
-                    </strong>
-                </p>
+            <h2>Visão</h2>
+            <h3>
+                Atender pessoas de todas as idades com literaturas educativas, de auto-ajuda e proporcionando bem-estar a todos.
+            </h3>
 
 
-                <h2 class="meta">Meta</h2>
-                <p>
-                    <strong>
-                        <!-- Texto Pronto -->
-                    Atingir o maior número de pessoas em Santa Catarina, Paraná e Rio Grande do Sul
-                </strong>
-            </p>
-
-
+            <h2>Meta</h2>
+            <h3>
+                Atingir o maior número de pessoas em Santa Catarina, Paraná e Rio Grande do Sul
+            </h3>
+        </div>
+        
+        <div class="projetos-igreja">     
             <h2>Valores</h2>
-                    <p>
-                        <div class="DivPValores" id="Vida">
-                            <div class="DivValores">
-                                <b>
-                                Vida
-                                </b>
-                            </div>
-                            
-                            <div class="DivValores">
-                                <b>
-                                Família
-                                </b>
-                            </div>
-                            
-                            <div class="DivValores">
-                                <b>
-                                Princípios Morais e éticos
-                                </b>
-                            </div>
-                            
-                            <div class="DivValores">
-                                <b>
-                                Educação
-                                </b>
-                            </div>
-                            
-                            <div class="DivValores">
-                                <b>
-                                Cultura
-                                </b>
-                            </div>
-                            
-                            <div class="DivValores">
-                                <b>
-                                Fé
-                                </b>
-                             </div>
+            <ul class="projeto-lista"> 
+                <li>Vida</li>
+                <li>Família</li>
+                <li>Princípios Morais e éticos</li>
+                <li>Educação</li>
+                <li>Cultura</li>
+                <li>Fé</li>
+                <li>Vida, Família</li>
 
-                            <p>
-                                <b>
-                                Vida, Família, <br> Princípios Morais e éticos,<br> Educação, Cultura, Fé
-                                </b>
-                            </p>
-                        </div>
-                    </p>
-                    <p class="pValores">
-                        <div class="DivPValores" id="Vida">
-                            <div class="DivValores">
-                                <b>
-                                Vida
-                                </b>
-                            </div>
-                            
-                            <div class="DivValores">
-                                <b>
-                                Família
-                                </b>
-                            </div>
-                            
-                            <div class="DivValores">
-                                <b>
-                                Princípios Morais e éticos
-                                </b>
-                            </div>
-                            
-                            <div class="DivValores">
-                                <b>
-                                Educação
-                                </b>
-                            </div>
-                            
-                            <div class="DivValores">
-                                <b>
-                                Cultura
-                                </b>
-                            </div>
-                            
-                            <div class="DivValores">
-                                <b>
-                                Fé
-                                </b>
-                             </div>
+        </div>
 
-                            <p>
-                                <b>
-                                Vida, Família, <br> Princípios Morais e éticos,<br> Educação, Cultura, Fé
-                                </b>
-                            </p>
-                        </div>
-                    </p>
-
-                    <img src="https://diocesedesaojoaodelrei.com.br/wp-content/uploads/2017/08/familia-696x362.jpg">
-
-
-
-                    <h2>Estados que pretendemos distribuir literaturas:</h2>
-                <div class="Bandeiras" id="BandeiraSC">
-                    <img src="https://www.infoescola.com/wp-content/uploads/2009/12/bandeira-de-santa-catarina.jpg" width="30%;"
+        <a href="#" class="link-seja-mantenedor">Acesse aqui para ser um sócio-mantenedor!</a>
+        <div class="img-center">
+                <img src="https://diocesedesaojoaodelrei.com.br/wp-content/uploads/2017/08/familia-696x362.jpg">      
+        </div>
+          
+        <div class="projetos-igreja">   
+            <h2>Estados que pretendemos distribuir literaturas:</h2>
+                <img src="https://www.infoescola.com/wp-content/uploads/2009/12/bandeira-de-santa-catarina.jpg" width="30%;"
                     alt="Foto da Bandeira do Estado de Santa Catarina">
-                </div>
 
-                <div class="Bandeiras" id="BandeiraRS">
-                    <img src="https://images.educamaisbrasil.com.br/content/banco_de_imagens/guia-de-estudo/D/rio-grande-do-sul-geografia.jpg" width="30%;" alt="Foto da Bandeira do Estado do Rio Grande do Sul">
-                </div>
+
+                <img src="https://images.educamaisbrasil.com.br/content/banco_de_imagens/guia-de-estudo/D/rio-grande-do-sul-geografia.jpg" width="30%;" alt="Foto da Bandeira do Estado do Rio Grande do Sul">
+            
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Bandeira_do_Paran%C3%A1.svg/300px-Bandeira_do_Paran%C3%A1.svg.png" width="30%;" alt="Foto da Bandeira do Estado do Paraná">
                 
-                <div class="Bandeiras" id="BandeiraPR">
-                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Bandeira_do_Paran%C3%A1.svg/300px-Bandeira_do_Paran%C3%A1.svg.png" width="30%;" alt="Foto da Bandeira do Estado do Paraná">
-                </div>
-            </div>          
-<!--     <div class="projetos-igreja">
-        <h2>Nossos Projetos</h2>
-
-        <h3>Projeto Prática de Fé</h3>
-        <ul class="projeto-lista">
-            <li>Realização de cultos com ministração de louvores e palavra.</li>
-            <li>Realização de cultos com ensino da Palavra de Deus</li>
-            <li>Realização de cultos evangelísticos com distribuição de literatura.</li>
-            <li>Realização de orações da fé por necessidades de cada família.</li>
-        </ul>
-    </div>
-
-    <div class="projetos-igreja">
-        <h3>Projeto Educacional</h3>
-        <ul class="projeto-lista">
-            <p>Desenvolvem atividades com crianças, adolescentes e jovens visando fortalecer o Caráter de Cristo,
-                valores, princípios morais e éticos, patriotismo, recreação, acampamentos, prática de escotismo e
-                integração em busca de uma formação educacional por princípios. </p>
-            <br><br>
-            <li>Realização de Palestras educativas de socialização para todas as faixas etárias.</li>
-        </ul>
-    </div>
-
-    <div class="projetos-igreja">
-        <h3>Projeto Social</h3>
-        <ul class="projeto-lista">
-            <li>Distribuimos roupas usadas para pessoas carentes de nossa cidade e também nas localidades do interior do
-                município. </li>
-            <li>Distribuimos cestas básicas de alimento para famílias carentes de nossa cidade e também nas localidades
-                do interior.</li>
-        </ul>
-    </div>
-
-    <div class="projetos-mais-info">
-        <p>Mais informações na página de projetos: <a href="{{ route('projects.page') }}">Quero saber mais!</a></p>
-    </div>
-    <hr>
-
-    <div class="reunioes">
-        <h2>Nossas Reuniões</h2>
-        <p>Noite da Vitória <br>
-            Quintas-Feiras às 19:15 hs</p>
-        <p>Noite da Família <br>
-            Domingos às 19:00 hs</p>
-    </div>
- -->
+        </div>    
+        
 @endsection
 
 
