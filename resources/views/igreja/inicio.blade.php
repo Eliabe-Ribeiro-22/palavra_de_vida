6d0b952d8df18b86202741b7e0957b1638374b45
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2022-12-23 13:25:48 -0300

    CSS - Estilização botão Whats

--- resources/views/igreja/inicio.blade.php
+++ resources/views/igreja/inicio.blade.php
@@ -27,53 +27,51 @@
     <h2>Visão</h2>
     <h3>
         Atender pessoas de todas as idades com literaturas educativas, de auto-ajuda e proporcionando bem-estar a todos.
-    </h3>
+    </h3>   
 
     <h2>Missão</h2>
     <h3>
         Levar conhecimento, cultura, educação, fé, lazer e entretenimento!
     </h3>
 
-    <h2>Valores</h2>
-    <ul class="projeto-lista bold">
-        <li>Vida</li>
-        <li>Família</li>
-        <li>Princípios Morais e éticos</li>
-        <li>Educação</li>
-        <li>Cultura</li>
-        <li>Fé</li>
-        <li>Vida, Família</li>
-    </ul>
-
-</div>
-
-<div class="projetos-igreja">
     <h2>Objetivo</h2>
+        
     <h3>
         Arrecadar fundos financeiros para aquisição de literaturas, tais como:
     </h3>
+        
     <ul class="projeto-lista bold">
         <li><a>Bíblias</a></li>
-
         <li><a>Livros</a></li>
-
         <li><a>Revistas</a></li>
-
         <li><a>Folhetos</a></li>
-
         <li><a>Apostilas</a></li>
-
         <li><a>Livretos para crianças, adolescentes, jovens e adultos</a></li>
     </ul>
 
     <h4 class="no-bold">
         Sendo que todos os materiais adquiridas serão distribuídos gratuitamente a toda a sociedade, através de seus associados contribuintes.
     </h4>
-
 </div>
-<a href="#" class="link-seja-mantenedor">Acesse aqui para ser um sócio-mantenedor!</a>
-<div class="img-center">
-    <img src="https://diocesedesaojoaodelrei.com.br/wp-content/uploads/2017/08/familia-696x362.jpg">
+
+<div id="img-text">
+    <div class="projetos-igreja">
+
+        <h2>Valores</h2>
+        <ul class="projeto-lista bold">
+            <li>Vida</li>
+            <li>Família</li>
+            <li>Princípios Morais e éticos</li>
+            <li>Educação</li>
+            <li>Cultura</li>
+            <li>Fé</li>
+            <li>Vida, Família</li>
+        </ul>
+    </div>
+
+    <div class="img-center" id="imagem-valores">
+        <img src="https://diocesedesaojoaodelrei.com.br/wp-content/uploads/2017/08/familia-696x362.jpg">
+    </div>
 </div>
 
 <div class="projetos-igreja">
@@ -81,6 +79,8 @@
 
     <h3>Atingir o maior número de pessoas em Santa Catarina, Paraná e Rio Grande do Sul</h3>
 </div>
+
+<a href="{{ route('contact.page') }}" class="link-seja-mantenedor">Acesse aqui para ser um sócio-mantenedor!</a>
 
 <div class="projetos-igreja">
     <h2>Estados que pretendemos distribuir literaturas:</h2>
