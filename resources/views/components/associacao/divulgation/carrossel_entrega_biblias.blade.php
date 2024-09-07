a6fb0d7e66a26ef1f4296740e5fd20275012b316
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2024-09-07 19:59:39 -0300

    PHP - Componentização carrossel entrega de bíblias da página divulgação

---
+++ resources/views/components/associacao/divulgation/carrossel_entrega_biblias.blade.php
@@ -0,0 +1,165 @@
+<div>
+    <h2 id="divulgacao-title">Entregas de Bíblias a pessoas</h2>
+{{-- 
+    + ---------------------------------- +
+    | Componente do carrossel de imagens |
+    | presente na página divulgação.     |
+    + ---------------------------------- +
+--}}
+
+{{-- CSS e JS do carrossel --}}
+<link rel="stylesheet" href="/assets/css/carrossel/carrossel.css">
+<script type="text/javascript" src="/assets/js/carrossel.js"></script>
+
+    {{-- Carrosel com imagens de entrega de bíblias pelo pastor 1--}}
+    <div class="container">
+        <button class="arrow-left control" aria-label="Previous image">&#8592;</button>
+        <button class="arrow-right control" aria-label="Next image">&#8594;</button>
+        <div class="gallery-wrapper">
+            <div class="gallery">
+                <img class="item" src="/assets/img/entrega-biblias/pastor-entrega-biblias.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item current-item" src="/assets/img/entrega-biblias/pastor-entrega-biblias-2.jpeg"
+                 alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/pastor-entrega-biblias-3.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/pastor-entrega-biblias-4.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/pastor-entrega-biblias-5.jpeg"
+            alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/pastor-entrega-biblias-6.jpeg" alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/pastor-entrega-biblias-7.jpeg"
+               alt="Pastor Volni entregando Bíblias a pessoas">
+            </div>
+        </div>
+    </div>
+
+{{-- Script do carrossel --}}
+<script>
+    slide();
+</script>
+
+    <p class="p-divulgacao">
+    Entrega de Bíblias pelo Pastor Volni a pessoas que não tinham Bíblia.
+    Pastor Volni é o tesoureiro da Asssociação Palavra de Vida
+    </p>
+
+
+{{-- Carrosel com imagens de entrega de bíblias 2 --}}
+    <script type="text/javascript" src="/assets/js/carrossel.js"></script>
+    <div class="container">
+        <button class="arrow-left control" aria-label="Previous image">&#8592;</button>
+        <button class="arrow-right control" aria-label="Next image">&#8594;</button>
+        <div class="gallery-wrapper">
+            <div class="gallery">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-8.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item current-item" src="/assets/img/entrega-biblias/entrega-biblias-9.jpeg"
+                 alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-10.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-11.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-12.jpeg"
+            alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-13.jpeg" alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-14.jpeg"
+               alt="Pastor Volni entregando Bíblias a pessoas">
+            </div>
+        </div>
+    </div>
+
+{{-- Script do carrossel --}}
+<script>
+    slide();
+</script>
+
+
+
+    <p class="p-divulgacao">
+        Entrega de Bíblias por associados a pessoas que não tinham Bíblia
+    </p>
+
+    {{-- Carrosel com imagens de entrega de bíblias  3 --}}
+    <script type="text/javascript" src="/assets/js/carrossel.js"></script>
+    <div class="container">
+        <button class="arrow-left control" aria-label="Previous image">&#8592;</button>
+        <button class="arrow-right control" aria-label="Next image">&#8594;</button>
+        <div class="gallery-wrapper">
+            <div class="gallery">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-15.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item current-item" src="/assets/img/entrega-biblias/entrega-biblias-16.jpeg"
+                 alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-17.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-18.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-19.jpeg"
+            alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-20.jpeg" alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-21.jpeg"
+               alt="Pastor Volni entregando Bíblias a pessoas">
+            </div>
+        </div>
+    </div>
+
+{{-- Script do carrossel --}}
+<script>
+    slide();
+</script>
+
+
+    {{-- Carrosel com imagens de entrega de bíblias  4 --}}
+    <script type="text/javascript" src="/assets/js/carrossel.js"></script>
+    <div class="container">
+        <button class="arrow-left control" aria-label="Previous image">&#8592;</button>
+        <button class="arrow-right control" aria-label="Next image">&#8594;</button>
+        <div class="gallery-wrapper">
+            <div class="gallery">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-22.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item current-item" src="/assets/img/entrega-biblias/entrega-biblias-23.jpeg"
+                 alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-24.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-25.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-26.jpeg"
+            alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-27.jpeg" alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-28.jpeg"
+               alt="Pastor Volni entregando Bíblias a pessoas">
+            </div>
+        </div>
+    </div>
+
+    {{-- Carrosel com imagens de entrega de bíblias  5 --}}
+    <script type="text/javascript" src="/assets/js/carrossel.js"></script>
+    <div class="container">
+        <button class="arrow-left control" aria-label="Previous image">&#8592;</button>
+        <button class="arrow-right control" aria-label="Next image">&#8594;</button>
+        <div class="gallery-wrapper">
+            <div class="gallery">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-29.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item current-item" src="/assets/img/entrega-biblias/entrega-biblias-30.jpeg"
+                 alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-31.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-32.jpeg"
+                alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-33.jpeg"
+            alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-34.jpeg" alt="Pastor Volni entregando Bíblias a pessoas">
+                <img class="item" src="/assets/img/entrega-biblias/entrega-biblias-35.jpeg"
+               alt="Pastor Volni entregando Bíblias a pessoas">
+            </div>
+        </div>
+    </div>
+
+    <p class="p-divulgacao">
+    Mais fotos de entrega de Bíblias por associados a pessoas que não tinham Bíblia
+</p>
+
+</div>
