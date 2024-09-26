ab92095762bb0d4fe647c403f46b5a21d2615216
Eliabe Ribeiro Mota <eliaberibeiro06@hotmail.com>
2024-09-26 10:32:23 -0300

    HTML - Correções página sobre_nos26/09

--- resources/views/associacao/sobre_nos.blade.php
+++ resources/views/associacao/sobre_nos.blade.php
@@ -12,9 +12,9 @@
 
     Temos a meta de arrecadar fundos para <strong>adquirir literaturas a serem distribuídas gratuitamente</strong> e realizar atividades sociais a pessoas carentes. <br>
 
-    Nossa história inicia no <strong>ano de 2023</strong>, quando o <strong>Pastor Volni</strong>, juntamente com <strong>Jacy Faust</strong>, atual presidente da associação, decidem criar uma associação para distribuições de literaturas, a exemplo da Bíblia Sagrada. <br>
+    Nossa história inicia no <strong>ano de 2023</strong>, quando o <strong>Pastor Volni</strong>, juntamente com <strong>Ev. Jacy Faust</strong>, atual presidente da associação, decidem criar uma associação para distribuições de literaturas, a exemplo da Bíblia Sagrada. <br>
 
-    Porém o <strong>Pastor Volni</strong>, atual tesoureiro da Associação Palavra de Vida, realiza distribuição de Bíblias há mais de duas décadas por onde quer que esteve pastorando, seja em cidades como Blumenau e Lages, como também em Campo Belo do Sul, Capão Alto e Ponte Alta do Norte.<br>
+    Porém o <strong>Pastor Volni</strong>, atual tesoureiro da Associação Palavra de Vida, realiza distribuição de Bíblias há mais de duas décadas por onde quer que esteve pastorando, seja em cidades como Lages, Blumenau como também em Capão Alto, Campo Belo do Sul e Ponte Alta do Norte.<br>
 
 
     <br> <br>
@@ -23,16 +23,16 @@
 
  <h2 class="text_center" id="subtitle_page_sobre">Assista aos vídeos abaixo falando acerca da Associação</h2>
 
-<video controls loop class="video_page_sobre">
+<video controls class="video_page_sobre">
 	<source src="/assets/video/video-site.palavra-de-vida.wmv" type="video/wmv">
 	<source src="/assets/video/video-site.palavra-de-vida.mp4" type="video/mp4">
 	Seu navegador não suporta esse vídeo
 </video>
-<p class="text_center" id="text_page_sobre">Explicação da associação pelo tesoureiro Pastor Volni</p>
+<p class="text_center" id="text_page_sobre">Vídeo institucional por tesoureiro, Pastor Volni</p>
 
 
 
-<video controls loop class="video_page_sobre"> 
+<video controls class="video_page_sobre"> 
 	<source src="/assets/video/video-recebedor-biblia.wmv" type="video/wmv">
 	<source src="/assets/video/video-recebedor-biblia.mp4" type="video/mp4">
 	Seu navegador não suporta esse vídeo
